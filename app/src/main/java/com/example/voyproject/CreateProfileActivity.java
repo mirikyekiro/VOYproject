16da372a8a13c341f387d7864004a183210dd6c2
mirikyekiro <vnkrvolesya@gmail.com>
2024-01-16 23:39:18 +0900

    Create full start screen (no functions)

--- app/src/main/java/com/example/voyproject/CreateProfileActivity.java
+++ app/src/main/java/com/example/voyproject/CreateProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.voyproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,16 @@ public class CreateProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_profile);
+
+        Button btnBackToStartAct = (Button) findViewById(R.id.backToStartActivity);
+
+        btnBackToStartAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateProfileActivity.this, StartScreen.class));
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/voyproject/StartScreen.java
+++ app/src/main/java/com/example/voyproject/StartScreen.java
@@ -1,12 +1,7 @@
 package com.example.voyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.graphics.drawable.AnimationDrawable;
+import android.view.WindowManager;
 import android.os.Bundle;
 
 public class StartScreen extends AppCompatActivity {
@@ -14,14 +9,7 @@ public class StartScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_screen);
-        Button btnCreateProfile = (Button) findViewById(R.id.btnCreateProfile);
-
-        btnCreateProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(StartScreen.this, CreateProfileActivity.class));
-            }
-        });
+        setContentView(R.layout.main_activity);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager. LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
