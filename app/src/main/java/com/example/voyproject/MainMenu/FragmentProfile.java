46dd94deb72f945e0c4f3cab56a767b295564f45
mirikyekiro <vnkrvolesya@gmail.com>
2024-01-22 20:07:22 +0900

    TabSystem v2.0

--- app/src/main/java/com/example/voyproject/MainMenu/FragmentProfile.java
+++ app/src/main/java/com/example/voyproject/MainMenu/FragmentProfile.java
@@ -16,5 +16,7 @@ public class FragmentProfile extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
     }
 }
---
+++ app/src/main/java/com/example/voyproject/MainMenu/MyViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.voyproject.MainMenu;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MyViewPagerAdapter extends FragmentStateAdapter {
+
+    public MyViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new FragmentHome();
+            case 1:
+                return new FragmentCalculate();
+            case 2:
+                return new FragmentProfile();
+            default:
+                return new FragmentHome();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
